496dcc476a8870b89b2bd064f996dca3ca21d0f7
Marc Stanley <marcson909@gmail.com>
2020-10-25 13:46:00 -0500

    added compoents and resumeData

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import ResumeData from './Components/Resume/ResumeData';
+//import { faChevronRight, faChevronLeft, faCircle, faCheckCircle, faPlus } from '@fortawesome/free-solid-svg-icons';
+
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header resumeData={ResumeData}/>
+      <About resumeData={ResumeData}/>
+      <Resume resumeData={ResumeData}/>
+      <Portfolio resumeData={ResumeData}/>
+     
     </div>
   );
 }
---
+++ src/Components/Resume/ResumeData.js
@@ -0,0 +1,96 @@
+const resumeData = {
+  "imagebaseurl": "",
+  "name": "Marc Stanley",
+  "role": "Software Engineer",
+  "linkedInID": "",
+  "githubID": "",
+  "roleDescription": "Temporary role description",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "https://www.github.com",
+      "className": "fa fa-github"
+    },
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com",
+      "className": "fa fa-linkedin"
+    }
+  ],
+  "aboutme": "I'm an Air Force Veteran working on completing my B.S. in Computer Science and seeking a Software Engineer role",
+  "location": "Denver, USA",
+  "education": [
+    {
+      "universityName" : "Old Dominion University",
+      "specialization" : "Computer Science B.S.",
+      "monthGraduating": "May",
+      "yearGraduating" : "2022",
+      "gpa"            : "3.87"
+    }
+  ],
+  "work": [
+    {
+    "companyName":"Air Force",
+    "role"       :"NCOIC, Airfield Management Operations",
+    "startDate": "Aug 2020",
+    "endDate"  : "Aug 2021",
+    "Achievements":"NCO OTQ"
+    },
+    {
+    "companyName":"Air Force",
+    "role"       :"NCOIC, Airfield Management Training",
+    "startDate": "Feb 2018",
+    "endDate"  : "Aug 2020",
+    "Achievements":"NCO OTQ"
+    },
+    {
+    "companyName":"Air Force",
+    "role"       :"Airfield Management Operations Supervisor",
+    "startDate": "Aug 2020",
+    "endDate"  : "Aug 2021",
+    "Achievements":"NCO OTQ"
+    }
+  ],
+  "skillsDescription":"My skills placeholder",
+  "skills": [
+    {
+      "skillname":"Javascript"
+    },
+    {
+      "skillname":"Reactjs"
+    },
+    {
+      "skillname":"C++"
+    },
+    {
+      "skillname":"Git/Github"
+    },
+    {
+      "skillname":"HTML/CSS"
+    },
+  ],
+  "portfolio": [
+    {
+      "name":"project1",
+      "description":"short description",
+      "imgurl":"placeholder"
+    },
+    {
+      "name":"project2",
+      "description":"short description",
+      "imgurl":"placeholder"
+    },
+    {
+      "name":"project3",
+      "description":"short description",
+      "imgurl":"placeholder"
+    }
+  ]
+}
+
+export default resumeData;
